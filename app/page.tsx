14d03bd60427c4340f0a61fbf7702033ee80cfa0
Kent Phan <kentphan2000@gmail.com>
2023-02-27 18:34:16 -0800

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import { BrowserView, MobileView } from "react-device-detect";
 import info from "../info.json";
 
-export default function Home() {
+export default function HomePage() {
 	return (
 		<>
 			<MobileView>
-				<h1>This is rendered only on mobile</h1>
+				<main>
+					<nav className="flex justify-between text-2xl">
+						<h1 className="font-medium">{`${info.fullName}`}</h1>
+						<div>
+							<div className="flex">
+								<h1>About</h1>
+								<h1>Projects</h1>
+								<h1>Resume</h1>
+								<h1>Contact</h1>
+							</div>
+						</div>
+					</nav>
+					<div>Mobile Test</div>
+				</main>
 			</MobileView>
 			<BrowserView>
 				<main>
