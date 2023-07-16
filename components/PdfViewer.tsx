cd856db172d9db0dc796cf285ba51335ea75c963
Kent Phan <kentphan2000@gmail.com>
2023-07-16 03:48:15 -0700

    updates

---
+++ components/PdfViewer.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { Suspense, type FC } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+	"pdfjs-dist/build/pdf.worker.min.js",
+	import.meta.url
+).toString();
+
+type PdfViewerProps = { file: string };
+
+const PdfViewer: FC<PdfViewerProps> = (props) => {
+	const { file } = props;
+
+	return (
+		<Document file={file}>
+			<Page
+				pageNumber={1}
+				renderTextLayer={false}
+				renderAnnotationLayer={false}
+			/>
+		</Document>
+	);
+};
+
+export default PdfViewer;
