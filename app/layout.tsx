c021d6585f9e1a0c3c247ee39fce227a0d7c0c2d
Kent Phan <kentphan2000@gmail.com>
2023-07-19 01:38:54 -0700

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export const metadata = {
 	title: "Kent Phan",
-	description: "My personal website",
+	description: "It's just honest work",
 };
 
 export default function RootLayout({
@@ -12,7 +12,17 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className="dark">
-			<meta property="og:image" content="https://brittanychiang.com/og.png" />
+			<meta
+				property="og:image"
+				content="https://cdn.discordapp.com/attachments/941208408672067624/1131139972959830076/image.png"
+			/>
+			<meta name="twitter:title" content="Kent Phan" />
+			<meta name="twitter:description" content="It's just honest work" />
+			<meta
+				name="twitter:image"
+				content="https://cdn.discordapp.com/attachments/941208408672067624/1131139972959830076/image.png"
+			/>
+			<meta name="twitter:card" content="summary_large_image" />
 			<body className="dark:bg-background">{children}</body>
 		</html>
 	);
